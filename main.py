4713762c7622851b8dc604651cfd8cd0c8ba8464
Dennys Marcos <kardeenx@gmail.com>
2021-11-12 19:07:25 -0300

    Comments and documentation over login function

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-from rsa.pkcs1 import decrypt
+# Desacoplamento de funções
 from routines.login import routine as LOGIN_ROUTINE
 from routines.register import routine as REGISTER_ROUTINE
 
-# Base URL for API calls
+# URL base da API que está rodando no Flask.
+# Base URL for API Endpoints that are running on Flask.
 baseUrl = 'http://localhost:5000'
 
 
+# Recursive function that manages User input on routine selection.
+# Função recursiva que administra a entrada do usuário na seleção das rotinas.
 def procedureSelection():
-    print('')
     print(
         '[1] Logar com um usuário existente\n[2] Registrar um novo usuário.\n[3] Sair da aplicação.'
     )
 
+    # Receives user input and calls routines accordingly.
+    # Recebe a entrada do usuário para chamar as rotinas de acordo com o solicitado.
     procedure = input('> ')
 
     if procedure == '1':
+        # Login routine handles user authentication based on correct credentials given
         result = LOGIN_ROUTINE(baseUrl)
 
         if result['success']:
@@ -22,23 +27,27 @@ def procedureSelection():
             quit()
         else:
             print(result['message'])
+            print('')
             procedureSelection()
     elif procedure == '2':
+        # Login routine handles user register into the db
         result = REGISTER_ROUTINE(baseUrl)
         if result['success']:
             print('Usuário registrado com sucesso. Você pode logar agora.')
             procedureSelection()
         else:
             print(result['message'])
+            print('')
             procedureSelection()
     elif procedure == '3':
         print('A aplicação terminará agora.')
         quit()
     else:
+        print('')
         print('Você precisa selecionar uma das opções abaixo:')
         procedureSelection()
 
 
-# User Procedure Selection
+# First recursive function call
 print('Selecione a rotina desejada entrando com o valor numérico da operação:')
 procedureSelection()
--- routines/login.py
+++ routines/login.py
@@ -6,29 +6,34 @@ from getpass import getpass
 
 
 def routine(baseUrl):
-    # User Data input
+    # User Data Input as string
     print('Entre com as suas credenciais:')
     inputUsername = input("Usuário: ")
     inputPassword = getpass("Senha: ")
 
+    # If one of those are not provided, return the error to the user
+    # Se uma das opções não forem fornecidas, retorna o erro para o usuário
     if ((not inputUsername) or (not inputPassword)):
         return {
             'success': False,
             'message': 'Erro: Você precisa entrar com um usuário e senha.'
         }
 
+    # Stringify username data for it to be able to be sent over the network
     validationData = json.dumps({'username': inputUsername})
 
-    # Make the request
+    # Make the request to the validation endpoint
+    # This endpoint returns if the user exists.
+    # If it does, it also returns user 'public_key' as a pkcs PEM string
     print('Carregando...')
     result = requests.get(baseUrl + '/api/v1/user/validate',
                           headers={'content-type': 'application/json'},
                           data=validationData)
 
-    # Parse the result data into an dictionary
+    # Parse the request result data into an dictionary
     validation = result.json()
 
-    # quit()
+    # If the user exists::
     if (validation['status']):
         # Instantiate a RSA Public Key for the valid user
         public_key = rsa.PublicKey.load_pkcs1(validation['public_key'],
@@ -46,7 +51,8 @@ def routine(baseUrl):
         # Serialize credentials so it can be sent safely over then network
         stringCredentials = encryptedCredentials.decode('latin1')
 
-        # Create the user object
+        # Create the user object to be sent over the network
+        # Cria o objeto do usuário para ser enviado para a validação de credenciais
         user_object = {
             'public_key': validation['public_key'],
             'credentials': stringCredentials
